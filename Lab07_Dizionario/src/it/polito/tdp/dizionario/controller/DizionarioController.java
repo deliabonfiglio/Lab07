41976205d855d5dfaa8dfd3d032fdaf33046ffbf
Delia <Pietro@172.20.40.63>
2017-04-26 10:31:16 +0200

    ultimato

--- Lab07_Dizionario/src/it/polito/tdp/dizionario/controller/DizionarioController.java
+++ Lab07_Dizionario/src/it/polito/tdp/dizionario/controller/DizionarioController.java
@@ -1,18 +1,26 @@
 package it.polito.tdp.dizionario.controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import it.polito.tdp.dizionario.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class DizionarioController {
 
+	private Model model;
+	
 	@FXML
 	private ResourceBundle resources;
+	@FXML // fx:id="testo"
+	private Text testo; // Value injected by FXMLLoader
 	@FXML
 	private URL location;
 	@FXML
@@ -30,36 +38,55 @@ public class DizionarioController {
 
 	@FXML
 	void doReset(ActionEvent event) {
-		txtResult.setText("Reset!");
+		txtResult.clear();
+		inputParola.clear();
+		inputNumeroLettere.clear();
+		tuttoVisibile(false);
+	}
+	
+	void tuttoVisibile( boolean valore){
+		inputParola.setVisible(valore);
+		btnTrovaVicini.setVisible(valore);
+		btnTrovaGradoMax.setVisible(valore);
+		testo.setVisible(valore);
 	}
 
 	@FXML
 	void doGeneraGrafo(ActionEvent event) {
 
 		try {
-			txtResult.setText("Controller -- TODO!");
+			int dim = Integer.parseInt(inputNumeroLettere.getText());
+						
+			model.createGraph(dim);
+			this.tuttoVisibile(true);
 			
+			txtResult.setText("Grafo Creato con successo!");			
 		} catch (RuntimeException re) {
 			txtResult.setText(re.getMessage());
 		}
 	}
-
+//per ottenere il vertice del grafo con il grado massimo.
+	//visualizzare grado, il vertice e la lista dei suoi diretti vicini
 	@FXML
-	void doTrovaGradoMax(ActionEvent event) {
-		
+	void doTrovaGradoMax(ActionEvent event) {	
 		try {
-			txtResult.setText("Controller -- TODO!");
+			String maxDegree= model.findMaxDegree();
+			
+			txtResult.setText("Parola di grado massimo: "+maxDegree.toString());
 
 		} catch (RuntimeException re) {
 			txtResult.setText(re.getMessage());
 		}
 	}
-
+//per visualizzare tutte le parole direttamente connesse a quella inserita
 	@FXML
 	void doTrovaVicini(ActionEvent event) {
 		
 		try {
-			txtResult.setText("Controller -- TODO!");
+			String parola = inputParola.getText();
+			List<String> vicini = new ArrayList<String>(model.displayNeighbours(parola));
+			
+			txtResult.setText("Vicini della parola inserita: \n"+ vicini.toString());
 
 		} catch (RuntimeException re) {
 			txtResult.setText(re.getMessage());
@@ -73,6 +100,12 @@ public class DizionarioController {
 		assert inputParola != null : "fx:id=\"inputParola\" was not injected: check your FXML file 'Dizionario.fxml'.";
 		assert btnGeneraGrafo != null : "fx:id=\"btnGeneraGrafo\" was not injected: check your FXML file 'Dizionario.fxml'.";
 		assert btnTrovaVicini != null : "fx:id=\"btnTrovaVicini\" was not injected: check your FXML file 'Dizionario.fxml'.";
-		assert btnTrovaGradoMax != null : "fx:id=\"btnTrovaTutti\" was not injected: check your FXML file 'Dizionario.fxml'.";
+        assert btnTrovaGradoMax != null : "fx:id=\"btnTrovaGradoMax\" was not injected: check your FXML file 'Dizionario.fxml'.";
+        assert testo != null : "fx:id=\"testo\" was not injected: check your FXML file 'Dizionario.fxml'.";
+
+	}
+
+	public void setModel(Model model) {
+		this.model=model;		
 	}
 }
--- Lab07_Dizionario/src/it/polito/tdp/dizionario/model/Model.java
+++ Lab07_Dizionario/src/it/polito/tdp/dizionario/model/Model.java
@@ -3,22 +3,80 @@ package it.polito.tdp.dizionario.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jgrapht.Graphs;
+import org.jgrapht.UndirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+import it.polito.tdp.dizionario.db.WordDAO;
+
 public class Model {
+	private UndirectedGraph<String, DefaultEdge> grafo = new SimpleGraph<String, DefaultEdge>(DefaultEdge.class);
+	private WordDAO wdao = new WordDAO();
+	private int dim=0;
 
 	public List<String> createGraph(int numeroLettere) {
-
-		System.out.println("Model -- TODO");
-		return new ArrayList<String>();
+		dim = numeroLettere;
+		List<String> vertici = new ArrayList<String>(wdao.getAllWordsFixedLength(dim));
+		
+		//creo i vertici
+		for(String parola: vertici){
+			grafo.addVertex(parola);
+		}
+		
+		for(int i=0; i<vertici.size(); i++){
+			for(int j=i+1; j<vertici.size(); j++){
+				if(this.isDifferent(vertici.get(i),vertici.get(j) ))
+					grafo.addEdge(vertici.get(i),vertici.get(j));
+			}
+		}
+		
+		System.out.println(grafo.toString());
+		return vertici;
+	}
+	private boolean isDifferent(String parola1, String parola2){
+		int cnt=0;		
+		for(int i=0; i<dim; i++){
+				if(parola1.charAt(i)!=parola2.charAt(i))
+					cnt ++;				
+			}
+		
+		if(cnt==1)
+			return true;
+		
+		return false;
 	}
 
 	public List<String> displayNeighbours(String parolaInserita) {
-
-		System.out.println("Model -- TODO");
-		return new ArrayList<String>();
+		List<String> vicini = new ArrayList<String>(Graphs.neighborListOf(grafo, parolaInserita));
+		
+		System.out.format("\nNeighbours of %s: ", parolaInserita);
+		System.out.println(vicini);
+		
+		return vicini;
 	}
 
 	public String findMaxDegree() {
-		System.out.println("Model -- TODO");
-		return "";
+		/*
+		 * per vedere se il grafo è stato creato posso definirlo =null 
+		 * e poi controllo se è ancora ==null allora richiamo createGraph
+		 * altrimenti cerco il grado massimo
+		 * 
+		 * la soluzione che ho scelto è di disattivare gli altri 2 bottoni se non viene prima premuto createGraph
+		 */
+		List<String> vertici = new ArrayList<String>(wdao.getAllWordsFixedLength(dim));
+		int maxDegree = grafo.degreeOf(vertici.get(0));
+		String result="";
+	
+		for(String vertex: vertici){
+				if(maxDegree < grafo.degreeOf(vertex))
+					maxDegree = grafo.degreeOf(vertex);
+					result = vertex;
+			}
+		result += "\ndi grado: "+maxDegree +"\nCon vicini: "+ this.displayNeighbours(result).toString();
+				//grado, il vertice e la lista dei suoi diretti vicini
+		
+		System.out.println(result.toString());
+		return result;
 	}
 }
