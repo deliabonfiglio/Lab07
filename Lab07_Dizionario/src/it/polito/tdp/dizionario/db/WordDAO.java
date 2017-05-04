68e3c52635d2e23dcf8e695061fe00e0900d2907
Delia <Delia@Pcstudio>
2017-05-04 13:14:30 +0200

    aggiunta del metodo in sql per cercare le parole simili a quella inserita

--- Lab07_Dizionario/src/it/polito/tdp/dizionario/db/WordDAO.java
+++ Lab07_Dizionario/src/it/polito/tdp/dizionario/db/WordDAO.java
@@ -13,9 +13,42 @@ public class WordDAO {
 	 * Ritorna tutte le parole di una data lunghezza che differiscono per un solo carattere
 	 */
 	public List<String> getAllSimilarWords(String parola, int numeroLettere) {
-		
-		System.out.println("WordDAO -- TODO");
-		return new ArrayList<String>();
+
+		Connection conn = DBConnect.getInstance().getConnection();
+		String sql = "SELECT nome FROM parola WHERE LENGTH(nome) = ? AND nome LIKE ?";
+		PreparedStatement st;
+		List<String> parole = new ArrayList<String>();
+
+		try {
+
+			st = conn.prepareStatement(sql);
+			st.setInt(1, numeroLettere);
+
+			for (int i = 0; i < numeroLettere; i++) {
+				char[] word = parola.toCharArray();
+				word[i] = '_';
+				String built = "";
+				for (int j = 0; j < numeroLettere; j++) {
+					built += Character.toString(word[j]);
+				}
+
+				st.setString(2, built);
+				ResultSet res = st.executeQuery();
+
+				while (res.next()) {
+					if (!res.getString("nome").equals(parola))
+						parole.add(res.getString("nome"));
+				}
+
+			}
+			conn.close();
+			return parole;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Errore connessione al database");
+			throw new RuntimeException("Error Connection Database");
+		}
 	}
 
 	/*
@@ -32,7 +65,7 @@ public class WordDAO {
 			st = conn.prepareStatement(sql);
 			st.setInt(1, numeroLettere);
 			ResultSet res = st.executeQuery();
-
+			System.out.println(sql.toString());
 			List<String> parole = new ArrayList<String>();
 
 			while (res.next())
